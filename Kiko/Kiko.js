17b6b198772bd580ea05d23d60902da8e2ca419c
marifpane <96240207+marifpane@users.noreply.github.com>
2022-01-11 18:20:03 -0300

    FasesKiko

--- Kiko/Kiko.js
+++ Kiko/Kiko.js
@@ -31,12 +31,12 @@ function responder2(){
         }
     }
     if (resposta == 1){
-        alert("Ninguém gosta de dono da razão. Chiquinha deixou sua fama subir a cabeça e por isso acabou sendo rejeitada pela população.");
-        location.replace("../GameOver/GameOverKiko.html");
+        alert("Todos na casa percebem como o Kiko está se esforçando para mudar e por isso não colocam ele no paredão da semana.");
+        location.replace("../Kiko/KikoFase3.html");
    
     } else{
-        alert("Chiquinha faz jus ao seu apelido e se mostra uma pessoa sensata, não se envolvendo em brigas e nem se estressando a toa.");
-        location.replace("../Kiko/KikoFase3.html");
+        alert("Cansados de serem prejudicados pelas atitudes egoistas de Kiko, a casa se une para eliminá-lo na semana.");
+        location.replace("../GameOver/GameOverKiko.html");
     }
 }
 
@@ -50,11 +50,11 @@ function responder3(){
         }
     }
     if (resposta == 1){
-        alert("Dois pesos e duas medidas. O apelido de fada sensata é substituído pelo de Dona Florinda. Por isso o público decide que a próxima pessoa a ser eliminada é Chiquinha.");
+        alert("Kiko se mostra uma pessoa muito maquiavélica, e por isso o resto da casa decide colocá-lo no paredão. E com alto índice de rejeição Kiko é o mais novo eliminado.");
         location.replace("../GameOver/GameOverKiko.html");
    
     } else{
-        alert("Chiquinha se mostra uma participante sensata e justa, com uma trajetória impecável durante o programa. Por isso quando é a grande final, o público decide por consagrá-la a grande vencedora.");
+        alert("Toda a vila fica impressionada com a trajetória de Kiko dentro da casa, de todos foi o que mais mudou, se tornando uma pessoa melhor. Dessa forma, ele não só volta do paredão do quarto branco, como também é quem acaba ganhando a grande final e a tão disutada viagem.");
         location.replace("../VoceVenceu.html");
     }
 }
